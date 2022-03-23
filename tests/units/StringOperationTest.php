fc8050c78bfbeefad0fdaebe3f37f377a9de28d3
Konstantin Osipov <kosipov@MacBook-Air-Konstantin.local>
2022-03-23 17:57:36 +0300

    fix test position commit

--- tests/units/StringOperationTest.php
+++ tests/units/StringOperationTest.php
@@ -1,12 +1,12 @@
 <?php
 
-namespace Kosipov\StringTests;
+namespace Kosipov\StringTests\Tests\Unit;
 
+use Kosipov\StringTests\StringOperation;
 use PHPUnit\Framework\TestCase;
 
 class StringOperationTest extends TestCase
 {
-
     public function testRevertString_PassedAbc_ReturnCba()
     {
         $strOp = new StringOperation("abc");
